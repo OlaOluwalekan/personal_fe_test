6fcd5ca3c6639fde7cd661be20fb4228cc54410a
OlaOluwalekan <olalekanbello534@gmail.com>
2023-04-20 15:32:51 +0100

    testing fetch

--- confirm.js
+++ confirm.js
@@ -1,1 +1,13 @@
 console.log('confirm')
+
+const verify = async () => {
+  try {
+    const res = await fetch('http://localhost:9000/api/v1/subscribe/test')
+    const data = await res.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+verify()
